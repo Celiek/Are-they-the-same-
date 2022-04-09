205bc259a071e1e85e184fc0ba9770eac44058c1
Celiek <63073744+Celiek@users.noreply.github.com>
2022-04-09 11:30:25 +0200

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,171 +1,117 @@
-//package com.company;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.stream.IntStream;
-import java.util.Map;
-
-import static java.lang.Math.pow;
+import java.util.*;
+import java.util.stream.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        // System.out.println("Hello, World!");
+        // Strefa Testów:
 
-        // int[] a = new int[] { 0, 144, 19, 161, 19, 144, 19, 11 };
-        // int[] b = new int[] { 0, 14641, 20736, 361, 25921, 361, 20736, 361 };
-        // int[] a1 = new int[] { 2, 2, 3 };
-        // int[] b1 = new int[] { 4, 4, 9 };
+        int[] a = new int[] { 0, 144, 19, 161, 19, 144, 19, 11 };
+        int[] b = new int[] { 0, 14641, 20736, 361, 25921, 361, 20736, 361 };
+        int[] a1 = new int[] { 2, 2, 3 };
+        int[] b1 = new int[] { 4, 4, 9 };
         int[] a2 = new int[] { 11, 19, 19, 121, 144, 144, 161, 161 };
         int[] b2 = new int[] { 121, 361, 361, 361, 14641, 20736, 20736, 25921 };
-        // int[] a3 = new int[] {};
-        // int[] b3 = new int[] {};
+        int[] a3 = new int[] {};
+        int[] b3 = new int[] {};
         int[] a4 = new int[] { 0 };
         int[] b4 = new int[] { 0 };
         int[] a6 = new int[] { 11, 19, 19, 19, 121, 144, 144, 161, 1008 };
         int[] b6 = new int[] { 121, 361, 361, 14641, 20736, 20736, 25921, 36100 };
-        int[] a7 = new int[]{2,2,3};
-        int[] b7 = new int[]{4,9,9};
+        int[] a7 = new int[] { 2, 2, 3 };
+        int[] b7 = new int[] { 4, 9, 9 };
         int[] a8 = new int[] { -14, 0, 1, 19, 144, 161, 191, 195 };
         int[] b8 = new int[] { 0, 1, 196, 361, 20736, 25921, 36481, 38025 };
 
-
-        // System.out.println("Test 1: ");
-        // System.out.println(AreSame(a, b));
-        // System.out.println("Test 2 Wartosc ktora powinna wyjsc : True");
-        // System.out.println(AreSame(a1, b1));
-        // System.out.println("Test 3: ");
-        // System.out.println(AreSame(a2, b2));
-        // System.out.println("Test 4: ");
-        // System.out.println(AreSame(a3, b3));
-        // System.out.println("Test 5: ");
-        // System.out.println(AreSame(a4, b4));
-        // System.out.println("Test 6: ");
-        // System.out.println(AreSame(a6, b6));
-        System.out.println(":Test 7" );
+        System.out.println("Test 1: ");
+        System.out.println(AreSame(a, b));
+        System.out.println("Test 2 Wartosc ktora powinna wyjsc : True");
+        System.out.println(AreSame(a2, b2));
+        System.out.println("Test 3: ");
+        System.out.println(AreSame(a2, b2));
+        System.out.println("Test 4: ");
+        System.out.println(AreSame(a3, b3));
+        System.out.println("Test 5: ");
+        System.out.println(AreSame(a4, b4));
+        System.out.println("Test 6: ");
+        System.out.println(AreSame(a6, b6));
+        System.out.println(":Test 7");
         System.out.println(AreSame(a7, b7));
 
         // System.out.println("tEST 8 :");
         // System.out.print(AreSame(a8, b8));
     }
 
-    static boolean AreSame(int[] a, int[] b) {
-            Arrays.sort(a);
-            Arrays.sort(b);
-            //określa czy dana tablica jest podzielna przez drugą
-            int jest = 0;
-            //suma długośći obu tablic
-            int suma = IntStream.of(a).sum();
-            int sumb = IntStream.of(b).sum();
-            
-            LinkedHashSet<Integer> = new 
+    static boolean AreSame(int[] a, int[] b){
 
-            // dlugosci tablic a i b
-            int dla = a.length;
-            int dlb = b.length;
-
-            // for (int i = 0; i < a.length; i++) {
-            // System.out.println("Wartosc zmiennej a: " + a[i]);
-            // System.out.println("Wartosc zmiennej b: " + b[i]);
-            // }
-
-            // wyświetla sume wartośći poszczególnych tablic
-            System.out.println("Suma wartośći a " + suma);
-            System.out.println("Suma wartośći b " + sumb);
-
-            if (dla != dlb) {
-                System.out.println("Nie są jednkaowej długośći ");
-                return false;
-            } else if(a == null || b == null){
-                System.out.println("Tablice są puste");
-                return false; 
-            } else if( dla == 0 && dlb == 0){
-                System.out.println("Tablice mają tylko zero w sobie ");
-                return true;
-            }
-
-            // for (int i = 0; i < dla; i++) {
-            // System.out.println("Wartosc zmiennej a: " + a[i]);
-            // System.out.println("Wartosc zmiennej b: " + b[i]);
-            // }
-
-
-            if (suma > sumb) {
-                // sprawdzanie wszystkich elementów tablicy a
-                for (int j = 0; j < dla; j++) {
-                    for(int i = 0 ; i < dla ; i++){
-                        // Sprawdzanie po kolei czy kwadrat danej liczby
-                        // jest kwadratem licbzy z tablicy a
-                        if (pow(b[j], 2) == a[i]) {
-                            // System.out.println("Jest równa tablicy b");
-                            jest++;
-                            // Sprawdzanie czy wartość zmiennej a i b jest taka sama
-                        } else if (b[j] == 0 && a[i] == 0) {
-                            jest++;
-                            // alternatywne wyniki do warunków powyżej
-                        }
-                    }
-                }
-                // jeżeli sumy długości obu tablic sa jednakowe
-            } else if (suma == sumb) {
-                // sprawdzanie każdej kómórki pamieci
-                for (int j = 0; j < dla; j++) {
-                    for(int i = 0; i < dla; i++){
-                        if (pow(b[j], 2) == a[i]) {
-                            // sprawdzanie czy długość dla jest dłuższe od dlb
-                            jest++;
-                        } else if (b[j] == 0 && a[i] == 0) {
-                            jest++;
-                        }
-                    }
-                }
-            } else if (sumb > suma) {
-                for(int i = 0; i < dlb; i++){
-                    for(int j = 0; j < dlb; j++){
-                        if( pow(a[i] , 2) == b[j]){
-                            jest++;
-                        } else if(pow(a[i] , 2) == b[j]){
-                            jest ++;
-                        } else if( a[i] == 0 && b[i] == 0){
-                            jest++;
-                        }
-                    }
-                }
-                
-            } else{
-                System.out.println("Coś poszło nie tak");
-            }
-
-            // zwraca ostateczny wynik działania funkcji
-            if (jest == dla) {
-                return true;
-            }else if(jest == dlb){
-                return true;
-            }
-            else {
-                return false;
-            }
+        //Sprawdzanie podstawowych założeń
+        //programowych zdefioniowanych przez program
+        if(a.length != b.length){
+            System.out.println("Nie są jednakowej długośći");
+            return false; 
+        } else if(a.length ==0 && b.length ==0){
+            System.out.println("");
         }
 
-        // static int[] ridOFF(int[] a){
-        //     // int[] temp = new int[a.length];
-            
-        //     // int tempprarily = 0;
+        //Tymczasowe wartości potrzebne do zamiany int[] na Integer[]
+        //przez boxowanie
+        Integer[] aa = Arrays.stream( a).boxed().toArray(Integer[]::new);
+        Integer[] bb = Arrays.stream( b).boxed().toArray(Integer[]::new);
 
-        //     // for(int i = 0 ; i < a.length - 1; i++){
-        //     //     int curr = a[i];
+        //Zamiania Integer[] na Listę
+        //dodani do Arralist-a wartość z tablic aa i bb
+        // też zmienne pomocnicze
+        ArrayList<Integer> A = new ArrayList<Integer>(Arrays.asList(aa));
+        ArrayList<Integer> B = new ArrayList<Integer>(Arrays.asList(bb));
 
-        //     //     if(curr != a[i+1]){
-        //     //         temp[tempprarily++] = curr;
-        //     //     }
-        //     // }
-            
-        //     // temp[tempprarily++] = a[a.length - 1 ];
+        //Sortowanie ArrayList-ow
+        Collections.sort(A);
+        Collections.sort(B);
+        //Pomocnicze zmienne 
+        int dla = A.size();
+        int dlb = B.size();
+        int ilejest = 0;
+        int sumA =0;
+        int sumB =0;
+        
+        // Usuwanie dulpikatóww z Arraylist
+        // zapisywanie do zmiennej pomocniczej
+        ArrayList<Integer> newlistA = getRidOff(A);
+        ArrayList<Integer> newlistB = getRidOff(B);
+        
+        // zliczanie długości arraylisty 
+        // newlistA metodą advanced for
+        for(int i : newlistA){
+            sumA +=i;
+        }
 
-        //     LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>(Arrays.asList(a.length));
+        // zliczanie długości arraylisty
+        // newlistB metodą standardową for
+        for(int i = 0; i < newlistA.size(); i++){
+            sumB += newlistA.get(i);
+        }
 
-        //     // bez dyplikatów
-        //     Integer[] bezdup = linkedHashSet.toArray(new Integer[] {});
+        if(dla > dlb){
 
-        //     return bezdup;
-        // }
+        } else if(dla == dlb){
+
+        } else if(dlb > dla){
+
+        }
+
+        return false;
     }
+
+    public static <T> ArrayList<T> getRidOff(ArrayList<T> list){
+        // Tworzenie nowej hashmapy
+        Set<T> set = new LinkedHashSet<>();
+        // dodawniae elementu do hashmapy
+        set.addAll(list);
+
+        // czyszczenie wejściowej listy
+        list.clear();
+        // dodawanie elementów listy
+        // 
+        list.addAll(set);
+        return list;
+    }
+}
---
+++ src/com/company/OLD.java
@@ -0,0 +1,172 @@
+
+// package com.company;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.Map;
+
+import static java.lang.Math.pow;
+
+public class OLD {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+
+        // int[] a = new int[] { 0, 144, 19, 161, 19, 144, 19, 11 };
+        // int[] b = new int[] { 0, 14641, 20736, 361, 25921, 361, 20736, 361 };
+        // int[] a1 = new int[] { 2, 2, 3 };
+        // int[] b1 = new int[] { 4, 4, 9 };
+        int[] a2 = new int[] { 11, 19, 19, 121, 144, 144, 161, 161 };
+        int[] b2 = new int[] { 121, 361, 361, 361, 14641, 20736, 20736, 25921 };
+        // int[] a3 = new int[] {};
+        // int[] b3 = new int[] {};
+        int[] a4 = new int[] { 0 };
+        int[] b4 = new int[] { 0 };
+        int[] a6 = new int[] { 11, 19, 19, 19, 121, 144, 144, 161, 1008 };
+        int[] b6 = new int[] { 121, 361, 361, 14641, 20736, 20736, 25921, 36100 };
+        int[] a7 = new int[] { 2, 2, 3 };
+        int[] b7 = new int[] { 4, 9, 9 };
+        int[] a8 = new int[] { -14, 0, 1, 19, 144, 161, 191, 195 };
+        int[] b8 = new int[] { 0, 1, 196, 361, 20736, 25921, 36481, 38025 };
+
+        // System.out.println("Test 1: ");
+        // System.out.println(AreSame(a, b));
+        System.out.println("Test 2 Wartosc ktora powinna wyjsc : True");
+        System.out.println(AreSame(a2, b2));
+        // System.out.println("Test 3: ");
+        // System.out.println(AreSame(a2, b2));
+        // System.out.println("Test 4: ");
+        // System.out.println(AreSame(a3, b3));
+        // System.out.println("Test 5: ");
+        // System.out.println(AreSame(a4, b4));
+        // System.out.println("Test 6: ");
+        // System.out.println(AreSame(a6, b6));
+        System.out.println(":Test 7");
+        System.out.println(AreSame(a7, b7));
+
+        // System.out.println("tEST 8 :");
+        // System.out.print(AreSame(a8, b8));
+    }
+
+    static boolean AreSame(int[] a, int[] b) {
+        Arrays.sort(a);
+        Arrays.sort(b);
+        // określa czy dana tablica jest podzielna przez drugą
+        int jest = 0;
+        // suma długośći obu tablic
+        int suma = IntStream.of(a).sum();
+        int sumb = IntStream.of(b).sum();
+
+        //Usuwa powtarzające sie 
+        a = ridOFF(a);
+        b = ridOFF(b);
+
+        // dlugosci tablic a i b
+        int dla = a.length;
+        int dlb = b.length;
+
+        // for (int i = 0; i < a.length; i++) {
+        //     System.out.println("Wartosc zmiennej a: " + a[i]);
+        //     System.out.println("Wartosc zmiennej b: " + b[i]);
+        // }
+
+        // wyświetla sume wartośći poszczególnych tablic
+        // System.out.println("Suma wartośći a " + suma);
+        // System.out.println("Suma wartośći b " + sumb);
+
+        if (dla != dlb) {
+            System.out.println("Nie są jednkaowej długośći ");
+            return false;
+        } else if (a == null || b == null) {
+            System.out.println("Tablice są puste");
+            return false;
+        } else if (dla == 0 && dlb == 0) {
+            System.out.println("Tablice mają tylko zero w sobie ");
+            return true;
+        }
+
+        // for (int i = 0; i < dla; i++) {
+        // System.out.println("Wartosc zmiennej a: " + a[i]);
+        // System.out.println("Wartosc zmiennej b: " + b[i]);
+        // }
+
+        if (suma > sumb) {
+            // sprawdzanie wszystkich elementów tablicy a
+            for (int j = 0; j < dla; j++) {
+                for (int i = 0; i < dla; i++) {
+                    // Sprawdzanie po kolei czy kwadrat danej liczby
+                    // jest kwadratem licbzy z tablicy a
+                    if (pow(b[j], 2) == a[i]) {
+                        // System.out.println("Jest równa tablicy b");
+                        jest++;
+                        // Sprawdzanie czy wartość zmiennej a i b jest taka sama
+                    } else if (b[j] == 0 && b[i] == 0) {
+                        jest++;
+                        // alternatywne wyniki do warunków powyżej
+                    }
+                }
+            }
+            // jeżeli sumy długości obu tablic sa jednakowe
+        } else if (suma == sumb) {
+            // sprawdzanie każdej kómórki pamieci
+            for (int j = 0; j < dla; j++) {
+                for (int i = 0; i < dla; i++) {
+                    if (pow(b[j], 2) == a[i]) {
+                        // sprawdzanie czy długość dla jest dłuższe od dlb
+                        jest++;
+                    } else if (b[j] == 0 && a[i] == 0) {
+                        jest++;
+                    }
+                }
+            }
+        } else if (sumb > suma) {
+            for (int i = 0; i < dlb; i++) {
+                for (int j = 0; j < dlb; j++) {
+                    if ( (int)pow(a[i], 2) == b[j]) {
+                        jest++;
+                    } else if (pow(a[i], 2) == b[j]) {
+                        jest++;
+                    } else if (a[i] == 0 && b[i] == 0) {
+                        jest++;
+                    }
+                }
+            }
+
+        } else {
+            System.out.println("Coś poszło nie tak");
+        }
+
+        // zwraca ostateczny wynik działania funkcji
+        if (jest == dla) {
+            return true;
+        } else if (jest == dlb) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Usuwanie powtórzeń z tablicy
+    //
+    //  
+    static int[] ridOFF(int[] a) {
+        int[] bezdup = new int[a.length];
+        int j = 0;
+        
+        for(int i= 0; i < a.length - 1; i++){
+            if(a[i] != a[i+1]){
+                bezdup[j++] = a[i];
+            }
+        }
+
+        bezdup[j++] = a[a.length-1];
+
+        List<Integer> lista = new ArrayList<Integer>(Arrays.asList(bezdup));
+        lista.removeAll(Arrays.asList(Integer.valueOf(0)));
+        bezdup = lista.toArray(new Integer[lista.size()]);
+
+        return bezdup;
+    }
+}
